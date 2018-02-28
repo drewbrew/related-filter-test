4b121cc589f190b8569fbb4f7fb7a80544ecbc13
Drew Winstel <drewbrew@users.noreply.github.com>
2018-02-28 13:11:15 -0600

    urls: wire in the routes

--- relatedtest/relatedtest/urls.py
+++ relatedtest/relatedtest/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+
+router.register(r'books', views.BookViewSet)
+router.register(r'chapters', views.ChapterViewSet)
+router.register(r'authors', views.AuthorViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
 ]
