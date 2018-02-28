646d8c53ff4f6e17cbabab9cb6adb52ee78898ea
Drew Winstel <drewbrew@users.noreply.github.com>
2018-02-28 13:11:13 -0600

    serializers: add basic serializers

---
+++ relatedtest/relatedtest/serializers.py
@@ -0,0 +1,31 @@
+"""proof-of-concept serializers"""
+
+from rest_framework import serializers
+
+from relatedtest import models
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = models.Author
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Book
+        fields = '__all__'
+
+
+class ChapterSerializer(serializers.ModelSerializer):
+
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+        return AuthorSerializer(self.book.author)
+
+    class Meta:
+        model = models.Chapter
+        fields = '__all__'
